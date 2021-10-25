ed6736b6ffb7c751a8a9740cfe465c5aa89c628d
Nafiz Rifat <imtiaz_rifat@yahoo.com>
2021-10-25 07:22:29 -0500

    Product class and associated unit test added

---
+++ eShop.BLLTests/ProductTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using eShop.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShop.BLL.Tests
+{
+    [TestClass()]
+    public class ProductTests
+    {
+        [TestMethod()]
+        public void SayHelloTest()
+        {
+            //Arrange
+            var currentProduct = new Product();
+            currentProduct.ProductName = "Milk - Whole 1G";
+            currentProduct.ProductId = 1;
+            currentProduct.Description = "1 gallon Whole Milk with Vitamin D";
+
+            var expected = "Hello Milk - Whole 1G (1) 1 gallon Whole Milk with Vitamin D";
+
+            //Act
+            var actual = currentProduct.SayHello();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
